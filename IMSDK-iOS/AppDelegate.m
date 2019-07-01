a4b179716ddf99136e3314a3b505077439639648
Lidalu <lidalu1125@aliyun.com>
2019-07-01 18:22:07 +0800

    Update AppDelegate.m

--- IMSDK-iOS/AppDelegate.m
+++ IMSDK-iOS/AppDelegate.m
@@ -228,7 +228,7 @@ void InitCrashReport()
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
+    [QIMKit setQIMProjectType:2];
     [self applicationInit];
     [[QIMKit sharedInstance] chooseNewData:YES];
     return YES;
